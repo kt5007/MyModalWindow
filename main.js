bdd69d8d1c75359a29c4beec749f06c81c96b9a2
kt5007 <kenjin.0546@gmail.com>
2021-07-13 22:28:12 +0900

    finnished lesson

---
+++ main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+{
+  const open = document.getElementById("open");
+  const close = document.getElementById("close");
+  const modal = document.getElementById("modal");
+  const mask = document.getElementById("mask");
+  
+  open.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+    mask.classList.remove("hidden");
+  });
+  close.addEventListener("click", () => {
+    modal.classList.add("hidden");
+    mask.classList.add("hidden");
+  });
+  mask.addEventListener("click", () => {
+    // modal.classList.add('hidden');
+    // mask.classList.add('hidden');
+    close.click(); //上と同じ処理
+  });
+}
